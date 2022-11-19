d9e658334652e2b1fa3425d148b4fee76cccfe55
Creeeeger <104427569+Creeeeger@users.noreply.github.com>
2022-11-19 11:13:49 +0100

    feat: generate simulated mining identifiers

--- Wallet_Miner.py
+++ Wallet_Miner.py
@@ -1,3 +1,5 @@
+import random
+import string
 import hashlib
 import binascii
 import base58
@@ -15,4 +17,12 @@ def is_valid_wallet(wallet):
     for _ in range(1, 3):
         hash_value = hashlib.sha256(binascii.unhexlify(hash_value)).hexdigest()
     return checksum == hash_value[:8]
+
+# Generate a random wallet ID
+def generate_wallet_id(size=33, chars=string.ascii_uppercase + string.digits + string.ascii_lowercase):
+    return "".join(random.choice(chars) for _ in range(size))
+
+# Generate a random transaction ID
+def generate_transaction_id(size=30, chars=string.digits + string.digits):
+    return "".join(random.choice(chars) for _ in range(size))
 
